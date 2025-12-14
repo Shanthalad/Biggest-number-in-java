99dfecd11a77f9a5b436c724d88689bda1a93f22
Lenovo <Lenovo@DESKTOP-PNAR7AQ>
2025-12-14 12:00:38 +0530

    Biggest Number in java

---
+++ src/com/biggest/Task3Biggest.java
@@ -0,0 +1,19 @@
+package com.biggest;
+
+public class Task3Biggest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int num1=19,num2=28;
+      if(num1==num2) 
+      System.out.println("not are equal number");
+   
+      else if(num1>num2)
+    	  System.out.println("the biggest number:"+num1);
+   
+      else
+     System.out.println("the biggest number:"+num2);
+      }
+      
+      }
+
